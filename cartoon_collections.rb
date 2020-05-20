a9aadce1643737f7b66bfe52989b97b69bbf1187
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-20 08:23:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,10 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
     
-  array.each_with_index {|val, index|
+  array.each_with_index {|item, index|
   actIndex = index + 1
   binding.pry
-    puts (actIndex  + ". Hello"  + val)
+    puts (actIndex  + ". Hello"  + item)
     
   }
 end
