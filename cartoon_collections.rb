100a7c9074887330ad5eeb4fac2f0e4b0d9e7b23
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-20 08:24:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,7 @@ def list_dwarves(array)
     
   array.each_with_index {|item, index|
   actIndex = index + 1
-  
-    puts (actIndex  + ". Hello"  + item)
+    puts (actIndex  + item)
     
   }
 end
